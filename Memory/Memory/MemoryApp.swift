aec86caa27ab48873dacdbf5c9d95fc4ffc49193
bbwark <96076911+bbwark@users.noreply.github.com>
2022-10-20 14:26:04 +0200

    First Commit
    Project Creation

---
+++ Memory/Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Bruno De Vivo on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
